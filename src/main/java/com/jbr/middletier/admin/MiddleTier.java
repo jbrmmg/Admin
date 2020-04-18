ddad54a0f1621dbec270c0c6fff2fff95178b6d2
jason <jason@jbrmmg.me.uk>
2020-04-18 11:18:59 +0100

    JBR-231 refactor admin

--- src/main/java/com/jbr/middletier/admin/MiddleTier.java
+++ src/main/java/com/jbr/middletier/admin/MiddleTier.java
@@ -1,15 +1,24 @@
 package com.jbr.middletier.admin;
 
+import com.jbr.middletier.admin.config.ApplicationProperties;
+import com.jbr.middletier.admin.config.DefaultProfileUtil;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableAdminServer
 @SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class MiddleTier {
-    public static void main(String[] args) {
-        SpringApplication.run(MiddleTier.class, args);
-    }
+    private static final Logger log = LoggerFactory.getLogger(MiddleTier.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MiddleTier.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        app.run(args);
+    }
 }
 
